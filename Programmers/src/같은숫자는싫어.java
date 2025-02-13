8d01c4391896c90848f57d799f29e220174c17a6
21379 <21379@KRK4OFLTP00644.kr.ds.amkor.com>
2025-02-13 16:04:04 +0900

    Initialize

---
+++ Programmers/src/같은숫자는싫어.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class 같은숫자는싫어 {
+
+	public static void main(String[] args) {
+		int[] input = {1,1,3,3,0,1,1};
+		
+		
+		for (int i : solution(input)){
+			System.out.println(i);
+		}
+	}
+	
+    public static int[] solution(int[] arr) {
+        int[] answer = {};
+        int[] input = arr;
+        int cnt = 0;
+        for(int i=1; i < input.length; i++) {
+            if(input[i] == input[i-1]){
+                input[i] = -1;
+                cnt+=1;
+            }else {
+                continue;
+            }
+        }
+        
+        answer = new int[input.length - cnt];
+        int index = 0;
+        for(int i=0; i < input.length; i++) {
+            if(input[i] != -1) {
+                answer[index] = input[i];
+                index++;
+            }else {
+                continue;
+            }
+        } 
+            
+        return answer;
+    }
+}
